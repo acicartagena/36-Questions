199719b2ee7aebe6d727bceb3975d176509581bb
Aci Cartagena <acicartagena@gmail.com>
2016-02-07 22:25:36 +1100

    update fonts

---
+++ Thirty Six Questions/CustomFont.swift
@@ -0,0 +1,13 @@
+//
+//  CustomFont.swift
+//  Thirty Six Questions
+//
+//  Created by Angela Cartagena on 7/02/2016.
+//  Copyright © 2016 Angela Cartagena. All rights reserved.
+//
+
+import Cocoa
+
+class CustomFont: UIFont {
+
+}
