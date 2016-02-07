f1ab912edf429c296917bf300f568a6f9b33d209
Aci Cartagena <acicartagena@gmail.com>
2016-02-07 22:25:51 +1100

    update look

--- Thirty Six Questions/CustomFont.swift
+++ Thirty Six Questions/CustomFont.swift
@@ -6,8 +6,54 @@
 //  Copyright © 2016 Angela Cartagena. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class CustomFont: UIFont {
+let fonts = [ "Alittlesunshine",
+                "appleberry",
+                "AtlanticCruise",
+                "BigNoodleTitling",
+                "BrainFlower",
+                "ClaphappyLight",
+                "DKLemonYellowSun",
+                "FISHfingers",
+                "homework",
+                "natwooddraws-Regular",
+                "orangejuice",
+                "RoundedElegance-Regular",
+                "ThrowMyHandsUpintheAir-Bold",
+                "TrashHand",
+                "TravelingTypewriter",
+                "WalkwayBlack",
+                "WalterGoesToAmerica",
+                "YouAreJustMyType",
+                "ComixLoud",
+                "LeGrimoireduBonheur",
+                "SteelfishOutline-Regular",
+                "WildShow"]
 
+extension UIFont {
+
+    class func randomFont() -> UIFont? {
+        let random = Int(arc4random_uniform(UInt32(fonts.count)))
+        let fontName = fonts[random]
+        
+        var fontSize: CGFloat = 36.0
+        
+        switch (fontName) {
+            case "WalterGoesToAmerica": fontSize = 52.0
+            case "TravelingTypewriter": fallthrough
+            case "ThrowMyHandsUpintheAir-Bold": fallthrough
+            case "RoundedElegance-Regular": fallthrough
+            case "WalkwayBlack":  fontSize = 32.0
+            default: break
+        }
+        
+        let font = UIFont(name: fontName, size: fontSize)
+        if font == nil {
+            print("font name: \(fontName)")
+        }
+        
+        return font
+    }
+    
 }
--- Thirty Six Questions/QuestionCardView.swift
+++ Thirty Six Questions/QuestionCardView.swift
@@ -20,15 +20,33 @@ class QuestionCardView: UIView {
         super.init(frame: CGRect.zero)
         
         self.addSubview(label)
-        label.font = UIFont.systemFontOfSize(36)
+        
+        label.text = card.cardString
+        if let custom = UIFont.randomFont() {
+            label.font = custom
+        }else {
+           label.font = UIFont.systemFontOfSize(36)
+        }
+        
+        if label.text!.characters.count > 120 {
+            label.font = UIFont(name: label.font.fontName, size: 30)
+        }
+        
+        print("text: \(label.text) font: \(label.font)")
+        
         label.textColor = UIColor.whiteColor()
         label.numberOfLines = 0
         label.textAlignment = .Center
-        label.text = card.cardString
+        
+        
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
         self.backgroundColor = RandomFlatColorWithShade(.Light)
         label.autoPinEdgeToSuperviewEdge(.Leading, withInset: 20)
         label.autoPinEdgeToSuperviewEdge(.Trailing, withInset: 20)
         label.autoAlignAxisToSuperviewAxis(.Horizontal)
+        label.autoPinEdgeToSuperviewEdge(.Top, withInset: 10, relation: .GreaterThanOrEqual)
+        label.autoPinEdgeToSuperviewEdge(.Bottom, withInset: 10, relation: .GreaterThanOrEqual)
         
         self.layer.cornerRadius = 20
     }
