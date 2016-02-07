8afc376904f6ed64f6e1122eb1078fb328227e1e
Aci Cartagena <acicartagena@gmail.com>
2016-02-07 17:30:45 +1100

    Initial Commit

---
+++ Thirty Six Questions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thirty Six Questions
+//
+//  Created by Angela Cartagena on 7/02/2016.
+//  Copyright © 2016 Angela Cartagena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
